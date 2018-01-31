b9fcf269b86ddb1962291181a66bd6cd1a7555d6
Matheus Campos <silva.campos.matheus@gmail.com>
2018-01-31 10:08:32 -0300

    Add files via upload

---
+++ fsm-infradehardware/fsm-infradehardware.ino
@@ -0,0 +1,137 @@
+#include "fsm_config.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const int buttonPin = D5;    // definicao do pino utilizado pelo botao
+const int ledPin = D7;       // definicao do pino utilizado pelo led
+const char* ssid = "jualabs"; // nome da rede wifi
+const char* password = "jualabsufrpe"; // senha da wifi
+const char* mqtt_server = "things.ubidots.com"; // url do servidor
+
+int buttonState = LOW;             // armazena a leitura atual do botao
+int lastButtonState = LOW;         // armazena a leitura anterior do botao
+unsigned long lastDebounceTime = 0;  // armazena a ultima vez que a leitura da entrada variou
+unsigned long debounceDelay = 50;    // tempo utilizado para implementar o debounce
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+// definicao das funcoes relativas a cada estado
+event connect_state(void) {
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+event idle_state(void) {
+  int tempoComeco = millis();
+  int tempoFinal = tempoComeco + 10000;
+  while (tempoComeco < tempoFinal) {
+    if(read_button()) {
+      return action;
+    }
+    tempoComeco = millis();
+  }
+
+  return empty;
+}
+
+event send_data_state(void) {
+  
+}
+
+event send_data_button_state(void) {
+  
+}
+
+event no_connection_state(void) {
+  
+}
+
+event end_state(void) {
+  
+}
+
+// variaveis que armazenam estado atual, evento atual e funcao de tratamento do estado atual
+state cur_state = ENTRY_STATE;
+event cur_evt;
+event (* cur_state_function)(void);
+
+// implementacao de funcoes auxiliares
+int read_button() {
+  int reading = digitalRead(buttonPin);
+
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+  
+  lastButtonState = reading;
+  
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (buttonState == HIGH) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  cur_state_function = state_functions[cur_state];
+  cur_evt = (event) cur_state_function();
+  if (EXIT_STATE == cur_state)
+    return;
+  cur_state = lookup_transitions(cur_state, cur_evt);
+}
